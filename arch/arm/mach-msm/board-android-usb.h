64f816ffcad34be6ab32ef511796b03a4e8317c6
Alexander Tarasikov <alexander.tarasikov@gmail.com>
2010-11-27 17:01:35 +0300

    Add the missing header for the android usb platform driver

---
+++ arch/arm/mach-msm/board-android-usb.h
@@ -0,0 +1,30 @@
+/* arch/arm/mach-msm/board-android-usb.h
+ *
+ * Copyright (c) 2010 htc-linux.org
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef __BOARD_ANDROID_USB_H__
+#define __BOARD_ANDROID_USB_H__
+
+#include <linux/platform_device.h>
+#include <asm/mach-types.h>
+#include <mach/msm_hsusb.h>
+#include <mach/msm_iomap.h>
+#include <asm/io.h>
+#include <linux/delay.h>
+
+#include <linux/usb/android_composite.h>
+
+#include "devices.h"
+
+#endif /* __BOARD_ANDROID_USB_H__ */
