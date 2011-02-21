d46be96355771deba920eb8a7ad6395007925fc3
Alexander Tarasikov <alexander.tarasikov@gmail.com>
2011-02-21 03:03:17 +0300

    Clear IRQs on init

--- arch/arm/mach-msm/irq.c
+++ arch/arm/mach-msm/irq.c
@@ -504,6 +504,8 @@ void __init msm_init_irq(void)
 		/* select IRQ for all INTs */
 		writel(0, VIC_INT_SELECT(n));
 
+		/* clear interrupts */
+		writel(0xffffffff, VIC_INT_CLEAR(n));
 		/* disable all INTs */
 		writel(0, VIC_INT_EN(n));
 	}
