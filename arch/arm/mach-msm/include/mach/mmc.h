273a66ae141c5b25dca49d4242371930c0cf760e
Alexander Tarasikov <alexander.tarasikov@gmail.com>
2011-03-24 01:41:29 +0300

    Add a typedef for callbacks in mmc.h (should this stupidity be purged at all?)

--- arch/arm/mach-msm/include/mach/mmc.h
+++ arch/arm/mach-msm/include/mach/mmc.h
@@ -8,6 +8,8 @@
 #include <linux/mmc/card.h>
 #include <linux/mmc/sdio_func.h>
 
+typedef void (*msm_mmc_callback)(int card_present, void *dev_id);
+
 struct embedded_sdio_data {
 	struct sdio_cis cis;
 	struct sdio_cccr cccr;
@@ -22,7 +24,7 @@ struct mmc_platform_data {
 	unsigned int (*status)(struct device *);
 	int sdcc_id;
 	struct embedded_sdio_data *embedded_sdio;
-	int (*register_status_notify)(void (*callback)(int card_present, void *dev_id), void *dev_id);
+	int (*register_status_notify)(msm_mmc_callback, void *dev_id);
 };
 
 #endif
