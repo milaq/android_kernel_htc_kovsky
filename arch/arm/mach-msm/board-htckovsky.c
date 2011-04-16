d279e7f8a3895e76aa460c7d2ca9d2590e03f817
Alexander Tarasikov <alexander.tarasikov@gmail.com>
2011-04-16 23:18:43 +0400

    Restore kovsky machine ident string

--- arch/arm/mach-msm/board-htckovsky.c
+++ arch/arm/mach-msm/board-htckovsky.c
@@ -933,7 +933,7 @@ static void __init htckovsky_fixup(struct machine_desc *desc, struct tag *tags,
 	}
 }
 
-MACHINE_START(HTCKOVSKY, "kovsky")
+MACHINE_START(HTCKOVSKY, "HTC Kovsky GSM phone (aka Xperia X1)")
 	.fixup = htckovsky_fixup,
 	.boot_params = 0x10000100,
 	.map_io = htckovsky_map_io,
