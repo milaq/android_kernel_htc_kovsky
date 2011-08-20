ae3e22268a1eb2156c8bef1cbd2f283b27015e51
Alexander Tarasikov <alexander.tarasikov@gmail.com>
2011-08-21 01:37:52 +0400

    Update MSM SDCC driver to the one from 3.1.0-rc2

--- arch/arm/mach-msm/include/mach/mmc.h
+++ arch/arm/mach-msm/include/mach/mmc.h
@@ -8,8 +8,6 @@
 #include <linux/mmc/card.h>
 #include <linux/mmc/sdio_func.h>
 
-typedef void (*msm_mmc_callback)(int card_present, void *dev_id);
-
 struct embedded_sdio_data {
 	struct sdio_cis cis;
 	struct sdio_cccr cccr;
@@ -17,13 +15,24 @@ struct embedded_sdio_data {
 	int num_funcs;
 };
 
-struct mmc_platform_data {
+struct msm_mmc_gpio {
+	unsigned no;
+	const char *name;
+};
+
+struct msm_mmc_gpio_data {
+	struct msm_mmc_gpio *gpio;
+	u8 size;
+};
+
+typedef void (*msm_mmc_callback)(int card_present, void *dev_id);
+
+struct msm_mmc_platform_data {
 	unsigned int ocr_mask;			/* available voltages */
-	int built_in;				/* built-in device flag */
 	u32 (*translate_vdd)(struct device *, unsigned int);
 	unsigned int (*status)(struct device *);
-	int sdcc_id;
 	struct embedded_sdio_data *embedded_sdio;
+	struct msm_mmc_gpio_data *gpio_data;
 	int (*register_status_notify)(msm_mmc_callback, void *dev_id);
 };
 
