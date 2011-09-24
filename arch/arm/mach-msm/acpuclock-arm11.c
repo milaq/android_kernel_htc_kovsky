c0ba6f53a682eb3c5aaeccdb19722ebebfb95331
Alexander Tarasikov <alexander.tarasikov@gmail.com>
2011-09-25 02:50:22 +0400

    Always set ACPU clock in power collapse/wfi regardless of requested frequency

--- arch/arm/mach-msm/acpuclock-arm11.c
+++ arch/arm/mach-msm/acpuclock-arm11.c
@@ -180,9 +180,8 @@ static int pc_pll_request(unsigned id, unsigned on)
 
 unsigned long acpuclk_power_collapse(void) {
 	int ret = acpuclk_get_rate();
-	if (ret > drv_state.power_collapse_khz)
-		acpuclk_set_rate(drv_state.power_collapse_khz * 1000, 1);
-	return ret;
+	acpuclk_set_rate(drv_state.power_collapse_khz * 1000, 1);
+	return ret * 1000;
 }
 
 unsigned long acpuclk_get_wfi_rate(void)
@@ -192,9 +191,8 @@ unsigned long acpuclk_get_wfi_rate(void)
 
 unsigned long acpuclk_wait_for_irq(void) {
 	int ret = acpuclk_get_rate();
-	if (ret > drv_state.wait_for_irq_khz)
-		acpuclk_set_rate(drv_state.wait_for_irq_khz * 1000, 1);
-	return ret;
+	acpuclk_set_rate(drv_state.wait_for_irq_khz * 1000, 1);
+	return ret * 1000;
 }
 
 static int acpuclk_set_vdd_level(int vdd)
