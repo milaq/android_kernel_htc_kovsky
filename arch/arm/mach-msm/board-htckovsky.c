d45c443bc446c41cb0cdb08f6728ff8caabfeb59
Thomas Poussevin <poussevin@gmail.com>
2012-01-31 09:26:04 +0100

    Board htckovsky : change ds2746 low voltage value from 3.6V to 3.35V.

--- arch/arm/mach-msm/board-htckovsky.c
+++ arch/arm/mach-msm/board-htckovsky.c
@@ -393,7 +393,7 @@ static struct ds2746_platform_data kovsky_battery_data = {
 	.resistance = 1500,
 	.capacity = 1660,
 	.high_voltage = 4200,
-	.low_voltage = 3600,
+	.low_voltage = 3350,
 	.block_charge = htckovsky_block_charge,
 };
 
